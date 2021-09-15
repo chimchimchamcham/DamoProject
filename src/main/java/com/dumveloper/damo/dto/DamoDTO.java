379fdc14784c1982ab160a94ff31ac55167cea10
hyesuelee <lhs75391@naver.com>
2021-09-15 13:52:24 +0900

    dto 생성중

--- src/main/java/com/dumveloper/damo/dto/DamoDTO.java
+++ src/main/java/com/dumveloper/damo/dto/DamoDTO.java
@@ -1,5 +1,36 @@
 package com.dumveloper.damo.dto;
 
-public class DamoDTO {
+import java.sql.Date;
 
+public class DamoDTO {
+	//회원정보
+	private String u_id;
+	private String u_pw;
+	private String u_name;
+	private String u_email;
+	private String u_nick;
+	private int u_age;
+	private int u_weight;
+	private int u_tarWeight;
+	private int u_height;
+	private boolean u_gender;
+	private boolean u_alarmYN;
+	private String u_intro;
+	private boolean u_adminYN;
+	private Date u_joinTm;
+	private String u_fileName;
+	private boolean u_blindYN;
+	private int u_chooseCnt;
+	private String g_code;
+	
+	//캘린더
+	private Date c_date;
+	private String c_content;
+	private int c_tarKcal;
+	private int c_tarExe;
+	
+	//일
+	
+	
+	
 }
