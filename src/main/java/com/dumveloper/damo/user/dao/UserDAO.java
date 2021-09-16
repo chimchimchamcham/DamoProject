d8c271afd7c2ab567a52f05024c9e37207d2da3d
dbwlgh <dbwlgh0929@naver.com>
2021-09-16 16:44:05 +0900

    user폴더에 올리기 로그인 완료

--- src/main/java/com/dumveloper/damo/user/dao/UserDAO.java
+++ src/main/java/com/dumveloper/damo/user/dao/UserDAO.java
@@ -1,9 +1,11 @@
 package com.dumveloper.damo.user.dao;
 
-
+import com.dumveloper.damo.dto.DamoDTO;
 
 public interface UserDAO {
 
 	int login(String id, String pw);
+
+	int join(DamoDTO dto);
 
 }
