bc3957b1cf5113c1270263a20c01d3cddf51669a
hyesuelee <lhs75391@naver.com>
2021-09-16 21:23:10 +0900

    달력 url 추가

--- src/main/java/com/dumveloper/damo/diary/controller/CalendarController.java
+++ src/main/java/com/dumveloper/damo/diary/controller/CalendarController.java
@@ -3,8 +3,18 @@ package com.dumveloper.damo.diary.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping(value="/calendar")
 public class CalendarController {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@RequestMapping(value="/")
+	public ModelAndView list() {
+		logger.info("리스트 요청");
+		return service.list();
+	}
+	
 }
