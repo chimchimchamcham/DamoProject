ac8bcfde94ba5fed07695c57bc1987a45a4320af
kimdonghwan <kimdonghwan626@gmail.com>
2021-09-16 21:46:38 +0900

    push

--- src/main/java/com/dumveloper/damo/fit/controller/FitController.java
+++ src/main/java/com/dumveloper/damo/fit/controller/FitController.java
@@ -2,15 +2,21 @@ package com.dumveloper.damo.fit.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dumveloper.damo.fit.service.FitService;
 
 @Controller
 public class FitController {
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired FitService fitservice;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
@@ -20,10 +26,13 @@ public class FitController {
 	}
 	
 	@RequestMapping(value = "/fitMain", method = RequestMethod.GET)
-	public String fitMain(Model model) {
-		
+	public ModelAndView fitMain() {
 		logger.info("지식핏 페이지 접속");
-		return "fit/fitMain";
+		
+		ModelAndView mav = new ModelAndView();
+		
+		//return "fit/fitMain";
+		return fitservice.fitMain();
 	}
 	
 	@RequestMapping(value = "/fitWriteForm", method = RequestMethod.GET)
--- src/main/java/com/dumveloper/damo/fit/service/FitService.java
+++ src/main/java/com/dumveloper/damo/fit/service/FitService.java
@@ -2,9 +2,25 @@ package com.dumveloper.damo.fit.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dumveloper.damo.fit.dao.FitDAO;
 
 @Service
 public class FitService {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired FitDAO fitdao;
+	
+	public ModelAndView fitMain() {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		//최신글의 지식핏 목록을 가져온다
+		
+		//해당 
+		return null;
+	}
 }
