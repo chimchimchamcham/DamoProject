15deec21811353785be8b7df8451317024191bfd
JeongSeulgi <emily1770@naver.com>
2021-09-18 18:04:12 +0900

    일기 하는중

--- src/main/java/com/dumveloper/damo/diary/service/CalendarService.java
+++ src/main/java/com/dumveloper/damo/diary/service/CalendarService.java
@@ -16,90 +16,80 @@ import com.dumveloper.damo.dto.DamoDTO;
 
 @Service
 public class CalendarService {
-	
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	@Autowired CalendarDAO dao;
-	
-	public ModelAndView list(String id) {
+   
+   Logger logger = LoggerFactory.getLogger(this.getClass());
+   
+   @Autowired CalendarDAO dao;
+   
+   public ModelAndView list(String id) {
 
-		LocalDate now = LocalDate.now();
-		logger.info("오늘 날짜 출력:{}",now);
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM");
-		String formatedNow = now.format(format);
-		logger.info("오늘 날짜 기준 달 출력 : {}",formatedNow);
-		
-		
-		ModelAndView mav = new ModelAndView();
-		
-		
-		if(id != null) {
-			ArrayList<DamoDTO>list = dao.list(id);
-			logger.info("작성 일기 갯수 : {}",list.size());
-			mav.addObject("list",list);
-<<<<<<< HEAD
-=======
-			mav.addObject("monthTarExe",monthData.get(0));
-			mav.addObject("monthTarKcal",monthData.get(1));
-			mav.addObject("monthContent",monthData.get(2));
-			
-			DamoDTO weightData = dao.getWeight(id);
-			
-			//weightData.getU_tarWeight() 
->>>>>>> 6e4d212d2182b9e3610ce84bef989a623cd0b8a6
-		}
-		
-		mav.setViewName("diary/calendar");
-		return mav;
-	}
-<<<<<<< HEAD
-
-	public HashMap<String, String> getMonthData(String id,String formattedDate) {
-		logger.info("요청한 달:{}",formattedDate);
-		HashMap<String, String> map = new HashMap<String, String>();
-=======
-	
-	//달 변경시 Ajax로 한달 목표 섭취, 운동 칼로리 와 목표 던져주는 메서드
-	public HashMap<String, String> getMonthDByAjax(String id,String formattedDate) {
-		logger.info("요청한 달:{}",formattedDate);
-		HashMap<String, String> map = new HashMap<String, String>();
-		ArrayList<String> list = getMonthData(id,formattedDate);
-		
-		//map에 담아주기
-		map.put("monthTarExe",list.get(0));
-		map.put("monthTarKcal",list.get(1));
-		map.put("monthContent", list.get(2));
-		
-;		return map;
-	}
-	
-	//한달 목표 섭취, 운동 칼로리 와 목표 가져오는 메서드
-	public ArrayList<String> getMonthData(String id,String formattedDate) {
-		ArrayList<String> list = new ArrayList<String>();
->>>>>>> 6e4d212d2182b9e3610ce84bef989a623cd0b8a6
-		DamoDTO dto = dao.getMonthData(id, formattedDate);
-		
-		//월 데이터가 없는 경우
-		String tarExe= "-";
-		String tarKcal= "-";
-		String content = "-";
-		
-		if(dto != null) {
-			tarExe = Integer.toString(dto.getC_tarExe());
-			tarKcal = Integer.toString(dto.getC_tarKcal());
-			content = dto.getC_content();
-		}
-		
-		logger.info("운동 칼로리:{}",tarExe);
-		logger.info("섭취 칼로리:{}", tarKcal);
-		logger.info("한달 목표:{}",content);
-		
-		map.put("monthTarExe",tarExe);
-		map.put("monthTarKcal",tarKcal);
-		map.put("monthContent", content);
-		
-		return map;
-	}
-	
-	
+      LocalDate now = LocalDate.now();
+      logger.info("오늘 날짜 출력:{}",now);
+      DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM");
+      String formatedNow = now.format(format);
+      logger.info("오늘 날짜 기준 달 출력 : {}",formatedNow);
+      
+      ModelAndView mav = new ModelAndView();
+      
+      if(id != null) {
+         ArrayList<DamoDTO>list = dao.list(id);
+         logger.info("작성 일기 갯수 : {}",list.size());
+         
+         ArrayList<String> monthData = getMonthData(id, formatedNow);
+         
+         mav.addObject("list",list);
+         mav.addObject("monthTarExe",monthData.get(0));
+         mav.addObject("monthTarKcal",monthData.get(1));
+         mav.addObject("monthContent",monthData.get(2));
+         
+         DamoDTO weightData = dao.getWeight(id);
+         
+         //weightData.getU_tarWeight() 
+      }
+      
+      mav.setViewName("diary/calendar");
+      return mav;
+   }
+   
+   //달 변경시 Ajax로 한달 목표 섭취, 운동 칼로리 와 목표 던져주는 메서드
+   public HashMap<String, String> getMonthDByAjax(String id,String formattedDate) {
+      logger.info("요청한 달:{}",formattedDate);
+      HashMap<String, String> map = new HashMap<String, String>();
+      ArrayList<String> list = getMonthData(id,formattedDate);
+      
+      //map에 담아주기
+      map.put("monthTarExe",list.get(0));
+      map.put("monthTarKcal",list.get(1));
+      map.put("monthContent", list.get(2));
+      
+;      return map;
+   }
+   
+   //한달 목표 섭취, 운동 칼로리 와 목표 가져오는 메서드
+   public ArrayList<String> getMonthData(String id,String formattedDate) {
+      ArrayList<String> list = new ArrayList<String>();
+      DamoDTO dto = dao.getMonthData(id, formattedDate);
+      
+      //월 데이터가 없는 경우
+      String tarExe= "-";
+      String tarKcal= "-";
+      String content = "-";
+      
+      if(dto != null) {
+         tarExe = Integer.toString(dto.getC_tarExe());
+         tarKcal = Integer.toString(dto.getC_tarKcal());
+         content = dto.getC_content();
+      }
+      
+      logger.info("운동 칼로리:{}",tarExe);
+      logger.info("섭취 칼로리:{}", tarKcal);
+      logger.info("한달 목표:{}",content);
+      
+      list.add(tarExe);
+      list.add(tarKcal);
+      list.add(content);
+      
+      return list;
+   }
+   
 }
