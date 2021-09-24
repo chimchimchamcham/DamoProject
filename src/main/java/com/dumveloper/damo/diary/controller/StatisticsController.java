58882bbe9d43e9117cba062e3c4a7c472a2038c8
kimdonghwan <kimdonghwan626@gmail.com>
2021-09-24 16:09:37 +0900

    ㄹㄷㅈㄹ

--- src/main/java/com/dumveloper/damo/diary/controller/StatisticsController.java
+++ src/main/java/com/dumveloper/damo/diary/controller/StatisticsController.java
@@ -1,10 +1,30 @@
 package com.dumveloper.damo.diary.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dumveloper.damo.diary.service.StatisticsService;
 
 @Controller
 public class StatisticsController {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired StatisticsService service;
+	
+	@RequestMapping(value="myChart/{dateData}")
+	 public HashMap<String,Object> myChart(@PathVariable String dateData,HttpSession session){
+		logger.info("통계 그래프 호출");
+		
+		String u_id = (String) session.getAttribute("loginId");
+		
+		return service.myChart(u_id,dateData);
+	}
 }
--- src/main/java/com/dumveloper/damo/diary/service/StatisticsService.java
+++ src/main/java/com/dumveloper/damo/diary/service/StatisticsService.java
@@ -2,9 +2,16 @@ package com.dumveloper.damo.diary.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.dumveloper.damo.diary.dao.StatisticsDAO;
 
 @Service
 public class StatisticsService {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired StatisticsDAO dao;
+	
+	
 }
